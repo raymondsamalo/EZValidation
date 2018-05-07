9db5192d5c98906fed89d486e1cc09b1a8ea0433
raymondsamalo <raymond.samalo@gmail.com>
2018-05-08 01:20:08 +0800

    Add Enum

---
+++ EZValidation/EZValidation/EnumValidatorExtension.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace EZValidation
+{
+    public class EnumValidatorCheck<TEnum> : ValidatorCheck<TEnum> 
+    {
+        /// <summary>
+        /// Gets the not null or empty.
+        /// </summary>
+        /// <value>The not null or empty.</value>
+        public EnumValidatorCheck<TEnum> IsDefined
+        {
+            get
+            {
+                this.Must(x => Enum.IsDefined(typeof(TEnum),x));
+                return this;
+            }
+        }
+    }
+    /// <summary>
+    /// Enum validator extension.
+    /// </summary>
+    public static class EnumValidatorExtension
+    {
+        /// <summary>
+        /// Adds the rule for.
+        /// </summary>
+        /// <returns>The rule for.</returns>
+        /// <param name="validator">Validator.</param>
+        /// <param name="ruleSelector">Rule selector.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
+        public static EnumValidatorCheck<TEnum> AddRuleFor<T,TEnum>(this Validator<T> validator, Func<T, TEnum> ruleSelector)
+        {
+            var rule = new ValidatorRule<T, TEnum>();
+            var check = new EnumValidatorCheck<TEnum>();
+            rule.Selector = ruleSelector;
+            rule.Check = check;
+            validator.Rules.Add(rule);
+            return check;
+        }
+    }
+}
--- EZValidation/EZValidation/StringValidatorExtension.cs
+++ EZValidation/EZValidation/StringValidatorExtension.cs
@@ -7,6 +7,10 @@ namespace EZValidation
     /// </summary>
     public class StringValidatorCheck : ValidatorCheck<string>
     {
+        /// <summary>
+        /// Gets the not null or empty.
+        /// </summary>
+        /// <value>The not null or empty.</value>
         public StringValidatorCheck NotNullOrEmpty
         {
             get
@@ -15,20 +19,39 @@ namespace EZValidation
                 return this;
             }
         }
+        /// <summary>
+        /// Ises the equals.
+        /// </summary>
+        /// <returns>The equals.</returns>
+        /// <param name="other">Other.</param>
         public StringValidatorCheck isEquals(String other)
         {
             this.Must(x => String.Equals(x, other));
             return this;
         }
+        /// <summary>
+        /// Longers the than.
+        /// </summary>
+        /// <returns>The than.</returns>
+        /// <param name="length">Length.</param>
         public StringValidatorCheck LongerThan(int length)
         {
             this.Must(x => x.Length > length);
             return this;
         }
     }
-
+    /// <summary>
+    /// String validator extension.
+    /// </summary>
     public static class StringValidatorExtension
     {
+        /// <summary>
+        /// Adds the rule for.
+        /// </summary>
+        /// <returns>The rule for.</returns>
+        /// <param name="validator">Validator.</param>
+        /// <param name="ruleSelector">Rule selector.</param>
+        /// <typeparam name="T">The 1st type parameter.</typeparam>
         public static StringValidatorCheck AddRuleFor<T>(this Validator<T> validator, Func<T, String> ruleSelector)
         {
             var rule = new ValidatorRule<T,string>();
@@ -38,6 +61,5 @@ namespace EZValidation
             validator.Rules.Add(rule);
             return check;
         }
-
     }
 }
